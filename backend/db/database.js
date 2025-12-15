2bf6ba486252149a7ad55c1990b5932aecf39576
jules-crevoisier <crevoisier.ju@gmail.com>
2025-12-15 16:50:16 +0100

    add | ajout de l'utilisation du .env

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath = path.join(__dirname, "cantine.db");
+const dbName = process.env.DB_NAME || "cantine.db";
+const dbPath = path.join(__dirname, dbName);
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Erreur connexion SQLite :", err.message);
   } else {
-    console.log("Connexion SQLite réussie");
+    console.log(`SQLite connecté : ${dbName}`);
   }
 });
 
